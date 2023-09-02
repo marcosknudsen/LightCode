5dfc142aff6de392c9aedb2c361121d634988281
Marcos Knudsen <marcosknu14@gmail.com>
2023-09-02 11:52:13 -0300

    id|PR logic implementation

--- FinishId.java
+++ FinishId.java
@@ -2,15 +2,24 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.util.HashMap;
 
-public class FinishId extends AccionSemantica{
+public class FinishId extends AccionSemantica {
 
     @Override
     public int ejecutar(BufferedReader codigoFuente, Lex lex, int caracterActual,
             HashMap<String, Simbolo> tablaSimbolos) throws IOException {
-        lex.setCadena(lex.getCadena()+String.valueOf((char)caracterActual));
-        tablaSimbolos.get(lex.getCadena());
-        // implementar logica identificador | PR
-        return -1;
+        Simbolo value = tablaSimbolos.get(lex.getCadena());
+        int Token;
+        lex.setCadena(lex.getCadena() + String.valueOf((char) caracterActual));
+        if (value == null) {
+            tablaSimbolos.put(lex.getCadena(), new Simbolo("String", "Var"));
+            Token = 45;
+        } else {
+            if (value.uso == "Var")
+                Token = 45;
+            else
+                Token = 46;
+        }
+        return Token;
     }
-    
+
 }
