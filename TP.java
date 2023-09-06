84eef35de361dd9697b1ced634350353c396a6df
marcosknudsen <84600432+marcosknudsen@users.noreply.github.com>
2023-09-06 11:36:00 -0300

    get all tokens on main

--- TP.java
+++ TP.java
@@ -3,6 +3,10 @@ import java.io.IOException;
 public class TP {
     public static void main(String[] args) throws IOException {
         Lex lex = new Lex(args[0]);
-        System.out.println(lex.getToken());
+        int Token;
+        do{
+            Token=lex.getToken();
+            System.out.println(Token);
+        }while(token!=-2)
     }
 }
