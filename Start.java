381ed297bfa8920c68e4a9662b320ded12c1f7f2
Marcos Knudsen <marcosknu14@gmail.com>
2023-09-07 19:37:04 -0300

    first char

--- Start.java
+++ Start.java
@@ -7,7 +7,7 @@ public class Start extends AccionSemantica{
     @Override
     public Pointer ejecutar(BufferedReader codigoFuente,Lex lex, int caracterActual,
             HashMap<String, Simbolo> tablaSimbolos, HashMap<String,Integer> tablaPRes) throws IOException {
-                lex.setCadena("");
+                lex.setCadena(String.valueOf((char)caracterActual));
         return new Pointer(-1);
     }
     
