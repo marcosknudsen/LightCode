e1753bf24d643849560e88a265a82d2f914c0e02
Marcos Knudsen <marcosknu14@gmail.com>
2023-09-07 12:38:55 -0300

    finishid fix

--- FinishId.java
+++ FinishId.java
@@ -6,21 +6,20 @@ public class FinishId extends AccionSemantica {
 
     @Override
     public Pointer ejecutar(BufferedReader codigoFuente, Lex lex, int caracterActual,
-            HashMap<String, Simbolo> tablaSimbolos, HashMap<String,Integer> tablaPRes) throws IOException {
+            HashMap<String, Simbolo> tablaSimbolos, HashMap<String, Integer> tablaPRes) throws IOException {
         Simbolo value = tablaSimbolos.get(lex.getCadena());
         boolean isExists = tablaPRes.containsKey(lex.getCadena());
-        int Token;
+        int Token=tablaPRes.get(lex.getCadena());
         lex.setCadena(lex.getCadena() + String.valueOf((char) caracterActual));
-        if (!isExists) {//Si no existe lo creo
-            value = new Simbolo("String", "Var");
-            tablaSimbolos.put(lex.getCadena(), value);
+        if (!isExists) {// Si no es PR reescribe el token
+            if (value == null) {
+                value = new Simbolo("String", "Var");
+                tablaSimbolos.put(lex.getCadena(), value);
+            }
+            Token=50;
         }
-        if (value.uso=="Var")
-            Token = 50;//Identificador
-        else //Palabra Reservada
-            Token=tablaPRes.get(lex.getCadena());   
-             
-    return new Pointer(Token, lex.getCadena());
+
+        return new Pointer(Token, lex.getCadena());
     }
 
 }
