a5a5a769d055ceb73dc5d6cd06db632de1ccde50
Marcos Knudsen <marcosknu14@gmail.com>
2023-09-07 23:50:32 -0300

    while on main

--- TP.java
+++ TP.java
@@ -3,10 +3,11 @@ import java.io.IOException;
 public class TP {
     public static void main(String[] args) throws IOException {
         Lex lex = new Lex("./a.txt");
-        int Token;
-        for (int i = 0; i <7;i++){
-            Token=lex.getToken();
+        int Token=0;
+        Token=lex.getToken();
+        do{
             System.out.println(Token);
-        }
+            Token=lex.getToken();
+        }while (Token!=-3);
     }
 }
