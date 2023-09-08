06dbca88d7a11995f8c490aaca12ae9b5b47760f
Marcos Knudsen <marcosknu14@gmail.com>
2023-09-08 14:16:25 -0300

    fix

--- FinishConstante.java
+++ FinishConstante.java
@@ -2,20 +2,17 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.util.HashMap;
 
-public class FinishConstante extends AccionSemantica{
+public class FinishConstante extends AccionSemantica {
 
     @Override
     public Pointer ejecutar(BufferedReader codigoFuente, Lex lex, int caracterActual,
             HashMap<String, Simbolo> tablaSimbolos, HashMap<String, Integer> tablaPRes) throws IOException {
-        try {
-            Simbolo symbol = tablaSimbolos.get(lex.getCadena());
-            if (symbol == null) 
-                tablaSimbolos.put(lex.getCadena(),new Simbolo(Integer.parseInt(lex.getCadena()) > 65535 ? "longint" : "uinteger", "Constante"));
-            lex.line++;
-            return new Pointer(55, lex.getCadena());
-        } catch (NumberFormatException e) {
-            System.out.println("Error en linea "+lex.line+": el valor numerico excede los rangos permitidos");
-            return new Pointer(-1, lex.getCadena());
-        }
+        Simbolo symbol = tablaSimbolos.get(lex.getCadena());
+        if (symbol == null)
+            tablaSimbolos.put(lex.getCadena(),
+                    new Simbolo(Integer.parseInt(lex.getCadena()) > 65535 ? "longint" : "uinteger", "Constante"));
+        codigoFuente.reset();
+        lex.line++;
+        return new Pointer(55, lex.getCadena());
     }
 }
