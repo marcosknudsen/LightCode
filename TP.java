2260fcbbd359611ee69c687691e603939c01d178
Marcos Knudsen <marcosknu14@gmail.com>
2023-09-08 15:36:40 -0300

    a

--- TP.java
+++ TP.java
@@ -2,7 +2,7 @@ import java.io.IOException;
 
 public class TP {
     public static void main(String[] args) throws IOException {
-        Lex lex = new Lex("./a.txt");
+        Lex lex = new Lex(args[0]);
         int Token=0;
         Token=lex.getToken();
         while (Token!=-1){
