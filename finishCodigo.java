6fee2abbdf622b59a70843eac1fd7f379b5fbbc4
Marcos Knudsen <marcosknu14@gmail.com>
2023-09-08 11:02:16 -0300

    finish on token -1

--- finishCodigo.java
+++ finishCodigo.java
@@ -7,7 +7,7 @@ public class finishCodigo extends AccionSemantica{
     @Override
     public Pointer ejecutar(BufferedReader codigoFuente, Lex lex, int caracterActual,
             HashMap<String, Simbolo> tablaSimbolos, HashMap<String, Integer> tablaPRes) throws IOException {
-        return new Pointer(-3);
+        return new Pointer(-1);
     }
     
 }
